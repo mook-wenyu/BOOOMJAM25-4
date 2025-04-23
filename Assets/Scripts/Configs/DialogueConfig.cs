23a46299934a496856538dad663c7d7c768605e8
XieyishengJimmy <thanklifetime@gmail.com>
2025-04-23 22:12:49 +0800

    提交新表格，表格修改

---
+++ Assets/Scripts/Configs/DialogueConfig.cs
@@ -0,0 +1,23 @@
+public class DialogueConfig : BaseConfig
+{
+    public string dialogueID;
+    public string speakerName;
+    public string speakerPic;
+    public int speakerPos;
+    public string dialogueText;
+    public string choice1Text;
+    public string choice1NextDialogue;
+    public string choice2Text;
+    public string choice2NextDialogue;
+    public string choice3Text;
+    public string choice3NextDialogue;
+    public int healthAdjust;
+    public int hungerAdjust;
+    public int energyAdjust;
+    public int spiritAdjust;
+    public string[] addBuff;
+    public string[] removeBuff;
+    public string defautNextDialogue;
+    public string nodeID;
+    public int nodeMap;
+}
---
+++ Assets/Scripts/Configs/Map3Config.cs
@@ -0,0 +1,20 @@
+public class Map3Config : BaseConfig
+{
+    public string name;
+    public string desc;
+    public int type;
+    public bool isOnMap;
+    public int[] mapLocation;
+    public bool isStartPoint;
+    public string[] unlocksMidNodes;
+    public string[] unlocksPostNodes;
+    public string nodeAfterComplete;
+    public string[] rewardIdGroup;
+    public int[] rewardAmountGroup;
+    public string[] requiredMaterialIdGroup;
+    public int[] requiredMaterialAmountGroup;
+    public double submitTime;
+    public string[] recipeIdGroup;
+    public string storyId;
+    public string path;
+}
